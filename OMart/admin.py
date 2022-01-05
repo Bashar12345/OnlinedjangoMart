0a4aaeab74efffecb13f09362976b57902ec4615
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-01-05 21:24:48 +0600

    init

--- OMart/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import auction_post
-
-# Register your models here.
-admin.site.register(auction_post)
--- OMart/app.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class OMartConfig(AppConfig):
-    name='OMart'
-
