1dbec0307fc475d30cef34c2f8d4d927c05da891
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-01-20 10:04:36 +0600

    init

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'OMart.apps.OmartConfig',
-    'users.apps.UserConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
