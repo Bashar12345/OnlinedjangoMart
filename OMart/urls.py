eecad7b85b32455a74589dc06d54f292aec8189a
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-01-20 11:09:05 +0600

    init

--- OMart/urls.py
+++ OMart/urls.py
@@ -2,11 +2,10 @@
 from unicodedata import name
 from django.urls import path, include
 from . import views
-from users import views as users_view
+
 
 urlpatterns = [
     path('', views.home, name='Omart-home'), #urls path address string 
     path('about/', views.about, name='Omart-about'),
-    path('register/', users_view.register,name="Omart_register"),
-
+    
 ]
--- django_project/urls.py
+++ django_project/urls.py
@@ -19,8 +19,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Omart/', include('OMart.urls')),
-    
-    #path('users/', include('OMart.urls')),
+    path('users/', include('users.urls')),
 
     # path('', include('userLog.urls')),
 
