c8b8b426f8bc1ddf26678a1cd20c72f17abf70e3
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-02-03 09:37:49 +0600

    init

--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
-from turtle import title
-from django.shortcuts import render
+# from turtle import title
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 
 def register(request):
     title='SignUp'
-    form = UserCreationForm()
-    return render(request,'users/register.html', {'form':form},{'title':title})
+
+    if request.method == 'POST':
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+          form.save()
+          username =form.cleaned_data.get('username') # username database e ase na need fixing
+          messages.success(request,f'Account created for {username}!')
+          return redirect('Omart-home')
+    else:
+      form=UserCreationForm()
+
+    return render(request,'users/register.html', {'form':form, 'title':title})
