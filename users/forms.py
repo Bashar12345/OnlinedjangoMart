59003a2251b00c4826159197d68418f06ac4c96b
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-02-04 09:42:10 +0600

    user registration update

---
+++ users/forms.py
@@ -0,0 +1,12 @@
+import email
+from email.policy import default
+from django import forms
+from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm
+
+class user_registration_form(UserCreationForm):
+    email= forms.EmailField() #default=True)
+
+    class Meta:
+        model = User
+        fields = ['username','email','password','confrimpassword']
--- users/views.py
+++ users/views.py
@@ -1,22 +1,23 @@
 # from turtle import title
 from pyexpat.errors import messages
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
+from .forms import user_registration_form
 
 def register(request):
     title='SignUp'
 
     if request.method == 'POST':
-      form = UserCreationForm(request.POST)
-      print(form)
+      form = user_registration_form(request.POST)
+      #print(form)
       if form.is_valid():
-          form.save()
+            print(form.cleaned_data)
+            form.save()
           # username =form.cleaned_data.get('username') # username database e ase na need fixing
-          username = form.cleaned_data['username']
-          print(username)
-          messages.success(request,f'Account created for {username}!')
-          return redirect('Omart-home')
+            username = form.cleaned_data['username']
+            print(username)
+            messages.success(request,f'Account created for {username}!')
+            return redirect('Omart-home')
     else:
-      form=UserCreationForm()
+      form = user_registration_form()
 
     return render(request,'users/register.html', {'form':form, 'title':title})
