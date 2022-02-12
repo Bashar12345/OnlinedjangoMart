03ce0a2a722db5cd714ac5c6cd0a143079a7f6be
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-02-12 23:08:02 +0600

    init

--- users/forms.py
+++ users/forms.py
@@ -8,7 +8,8 @@ from users.db_models import *
 
 
 class user_registration_form(forms.ModelForm):
-    #email = forms.EmailField(label='Enter your Email:', max_length=150)
+    email1 = forms.EmailField(
+        label='Enter your Email  testing :', max_length=150, widget=forms.TextInput(attrs={'class':'form-control-lg'}))
     #password = forms.CharField(widget=forms.PasswordInput) 
     # CHOICES = (('Option 1', 'Option 1'),('Option 2', 'Option 2'),)
     # field = forms.ChoiceField(choices=CHOICES)
@@ -21,11 +22,11 @@ class user_registration_form(forms.ModelForm):
         fields = ('name', 'email', 'password', 'confrim_password', 'house_no', 'address_line1',
                   'address_line2', 'telephone', 'zip_code', 'state', 'country',)  # 'instrument_purchase'
         labels = {'name': 'Enter your Fullname', 
-                  'email': 'Enter your Email Address',
+                  #'email': 'Enter your Email Address',
                   'address_line1': 'Present Address','address_line2':'Permanent Address'}
         widgets = {
                #'name': forms.TextInput(attrs={'class': 'form-control-lg'}),
-               'email': forms.TextInput(attrs={'class': 'form-control-lg'}), 
+               #'email': forms.TextInput(attrs={'class': 'form-control-lg'}), 
                'password': forms.PasswordInput(),
                'confrim_password' : forms.PasswordInput(),
                #'instrument_purchase':'',
