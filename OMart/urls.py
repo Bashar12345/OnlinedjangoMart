4a09d6f1e739c28710fb2f177624aa7347fc148c
Muhammad Abul Bashar <abul35-2002@diu.edu.bd>
2022-02-15 15:45:29 +0600

    init

--- OMart/urls.py
+++ OMart/urls.py
@@ -2,10 +2,11 @@
 from unicodedata import name
 from django.urls import path, include
 from . import views
+from users import views as user_views
 
 
 urlpatterns = [
     path('', views.home, name='Omart-home'), #urls path address string 
     path('about/', views.about, name='Omart-about'),
-    path
+    path('users/', user_views.register, name='Omart-register'),
 ]
--- django_project/settings.py
+++ django_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'OMart.apps.OmartConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
