06a6ab1192861614c59f304fd0a7baf9d8acafa4
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-02-23 00:42:16 +0600

    init

--- OMart/views.py
+++ OMart/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from products.models import product_info, auctioned_product
 
 # Create your views here.
+
 
 @login_required
 def home(request):
     title = "Homepage"
-    return render(request, 'OMart/home.html', {'title': title})
+    products = product_info.objects.all()
+    return render(request, 'OMart/home.html', {'title': title, 'products': products})
+
 
 @login_required
 def about(request):
     title = "About"
+
     return render(request, 'OMart/about.html', {'title': title})
 
 
