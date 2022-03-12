f39ec4909038f51e447257971433147ee949752f
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-03-12 10:59:01 +0600

    rest api added

--- django_project/settings.py
+++ django_project/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'omart_api.apps.OmartApiConfig',
 ]
 
 #AUTH_USER_MODEL = 'users.User'
---
+++ omart_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OmartApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'omart_api'
